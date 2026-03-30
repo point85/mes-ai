0a8d25cbe500ce9c8a140396c40b88af98867848
Kent Randall <point85.llc@gmail.com>
2026-03-30 08:26:30 -0700

    fixed migration

--- server/alembic/versions/20260330_1000_e5f6a7b8c9d0_remove_equipment_status_column.py
+++ server/alembic/versions/20260330_1000_e5f6a7b8c9d0_remove_equipment_status_column.py
@@ -4,15 +4,15 @@ Equipment availability is now determined solely by state machine state
 (PackML, SEMI E10, or user-defined). If no state machine is assigned,
 100% availability is assumed.
 
-Revision ID: d4e5f6a7b8c9
-Revises: c3d4e5f6a7b8
-Create Date: 2026-03-29 10:00:00.000000
+Revision ID: e5f6a7b8c9d0
+Revises: 8c7e3fa160fd
+Create Date: 2026-03-30 10:00:00.000000
 """
 
 from alembic import op
 
-revision = "d4e5f6a7b8c9"
-down_revision = "c3d4e5f6a7b8"
+revision = "e5f6a7b8c9d0"
+down_revision = "8c7e3fa160fd"
 branch_labels = None
 depends_on = None
 
