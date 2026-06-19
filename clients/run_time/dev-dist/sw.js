145f43948e045cb967d3704a88b04252df7ead8c
Kent Randall <point85.llc@gmail.com>
2026-06-18 19:04:03 -0700

    Fixed Dependabot alerts

--- clients/run_time/dev-dist/sw.js
+++ clients/run_time/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5e738vnbe68"
+    "revision": "0.p7u7ei0qr5c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
