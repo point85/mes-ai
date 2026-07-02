e49e9c02c69fd595a4e70df837eac355484d2aed
Kent Randall <point85.llc@gmail.com>
2026-07-02 11:27:08 -0700

    Version 1.5.1

--- server/src/mes/config.py
+++ server/src/mes/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     # --- General ---
     PROJECT_NAME: str = "MES AI"
-    VERSION: str = "1.5.0"
+    VERSION: str = "1.5.1"
     API_V1_STR: str = "/api/v1"
 
     # --- Database (DATA-LAYER) ---
